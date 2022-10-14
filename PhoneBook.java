6ce7f79a180977e1c8910b92427175090b15f2b3
Vivvianne <ndiahav@gmail.com>
2022-10-14 16:05:24 +0300

    adding the find object

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,4 +1,9 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
 import java.util.Scanner;
+import java.io.File;
 
 
 public class PhoneBook {
@@ -15,58 +20,102 @@ public class PhoneBook {
 	public static void saveContact(String name, long phoneNumber){
 		System.out.println("Saving contact " + name + " : " + phoneNumber);
 
+		try{
+			
+			PrintWriter pw = new PrintWriter(new FileWriter("file.txt", true));
+			pw.println(name + " : " + phoneNumber);
+			pw.close();
+
+
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+
+		}
+
+
+
+
+	
 
 	}
 
 	public static void findNumber(String name){
-		System.out.println("Could not find " + name);
 
+		try(Scanner in = new Scanner(new File("file.txt"))) {
+			String s[];
+
+			boolean foundPerson = false;
+
+			while(in.hasNextLine()) {
+				s = in.nextLine().split(":");
+				if(s[0].equals(name)) {
+					System.out.println("The number associated with " + name + " is " + s[1]);
+					foundPerson = true;
+				}
+
+				
+			}
+
+			if(!foundPerson){
+				System.out.println("Could not find " + name);
+			}
+
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+		
+		} 
 	}
+
+
+	
 
 	public static void displayMenu(){
 
-		Scanner in = new Scanner(System.in);
-		String name;
+		try (Scanner in = new Scanner(System.in)){
+			String name;
 
-		System.out.println("What would you like to do?");
-		System.out.println("1. Call contact.");
-		System.out.println("2. Save contact");
-		System.out.println("3. Find contact.");
+			System.out.println("What would you like to do?");
+			System.out.println("1. Call contact.");
+			System.out.println("2. Save contact");
+			System.out.println("3. Find contact.");
 
-		int choice = in.nextInt();
-		in.nextLine();
+			int choice = in.nextInt();
+			in.nextLine();
 
-		switch(choice){
-			case 1:
-				System.out.println("Who would you like to call? (FirstName LastName)");
-				name = in.nextLine();
+			switch(choice){
+				case 1:
+					System.out.println("Who would you like to call? (FirstName LastName)");
+					name = in.nextLine();
 
-				callContact(name);
-				break;
+					callContact(name);
+					break;
 
-			case 2:
-				System.out.println("\nWhat is the name of the person you would like to save? (Firstname LastName)");
-				name = in.nextLine();
+				case 2:
+					System.out.println("\nWhat is the name of the person you would like to save? (Firstname LastName)");
+					name = in.nextLine();
 
-				System.out.println("\nWhat is the phone number of the person you are saving?  (254)");
-				long phoneNumber = in.nextLong();
-				in.nextLine();
+					System.out.println("\nWhat is the phone number of the person you are saving?  (254)");
+					long phoneNumber = in.nextLong();
+					in.nextLine();
 
-				saveContact(name, phoneNumber);
+					saveContact(name, phoneNumber);
 
-				break;
+					break;
 
-			case 3:
-				System.out.println("what is the name of the person whose phone number you are searching for? (FirstName LastName)");
-				findNumber(in.nextLine());
-				break;
+				case 3:
+					System.out.println("what is the name of the person whose phone number you are searching for? (FirstName LastName)");
+					findNumber(in.nextLine());
+					break;
 
 
-			default:
+				default:
 
-				break;
+					break;
+			}
+
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
 		}
-
 
 	}
 }
